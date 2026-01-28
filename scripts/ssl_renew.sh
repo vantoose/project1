eddca20f15b19922f9ef649fe06392485181fdea
ivan <vantoose@gmail.com>
2026-01-28 18:04:54 +0500

    ..

--- scripts/ssl_renew.sh
+++ scripts/ssl_renew.sh
@@ -3,7 +3,7 @@
 COMPOSE="/usr/local/bin/docker-compose --ansi never"
 DOCKER="/usr/bin/docker"
 
-cd /home/www/
+cd /home/www/ishipilov.ru/
 $COMPOSE run certbot renew --dry-run
 $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
