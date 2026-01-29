ee6f15dd27cb045d4f9db4fc8ec62f4b0e13423d
ivan <vantoose@gmail.com>
2026-01-29 11:00:26 +0500

    ..

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -4,7 +4,6 @@
 <div class="container">
     <div class="list-group mb-3">
         <a href="{{ route('5bukv') }}" class="list-group-item list-group-item-action">{{ __('routes.web.5bukv') }}</a>
-        <a href="{{ route('hash') }}" class="list-group-item list-group-item-action">{{ __('routes.web.hash') }}</a>
     </div>
 
     @can('memos')
